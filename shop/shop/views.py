5adfbfd1ee8b58325e19285721f71527ad2e2ab4
AM KK <75850182+Endrju00@users.noreply.github.com>
2021-08-05 19:29:42 +0200

    Bootstrap and css added

--- shop/shop/views.py
+++ shop/shop/views.py
@@ -1,5 +1,13 @@
 from django.views.generic import TemplateView
 
+from items.models import SaleOffer
+
 # Create your views here.
 class HomePage(TemplateView):
     template_name = 'index.html'
+
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['categories'] = [x[1] for x in SaleOffer.CATEGORY]
+        return context
