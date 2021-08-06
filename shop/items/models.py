ab12efa656c6cdc9cfd6b1533244f3dd431b5f05
AM KK <75850182+Endrju00@users.noreply.github.com>
2021-08-06 14:07:12 +0200

    List of sale offers view added

--- shop/items/models.py
+++ shop/items/models.py
@@ -11,15 +11,34 @@ class Item(models.Model):
     height = models.PositiveIntegerField(help_text='Please pass the height in centimeters.')
     length = models.PositiveIntegerField(help_text='Please pass the length in centimeters.')
     width = models.PositiveIntegerField(help_text='Please pass the width in centimeters.')
+    color = models.CharField(max_length=50, default="Unknown")
+
+    NEW = 'N'
+    USED = 'U'
+    NOT_SPECIFIED = 'NS'
+    CONDITION = [
+        (NEW, 'New'),
+        (USED, 'Used'),
+        (NOT_SPECIFIED, 'Not Specified'),
+    ]
+    condition = models.CharField(max_length=2, choices=CONDITION, default=NOT_SPECIFIED)
 
     def __str__(self):
         return f"{self.name} {self.producer}"
+
+    def get_condition(self):
+        for c in self.CONDITION:
+            if c[0] == self.condition:
+                return c[1]
+        return 'Not Specified'
 
 
 class SaleOffer(models.Model):
     item = models.ForeignKey(Item, on_delete=models.CASCADE)
     price = models.FloatField()
     description = models.CharField(max_length=300)
+    quantity = models.PositiveIntegerField(default=1, help_text='Please pass the number of pieces in the package.')
+    free_delivery = models.BooleanField(default=False)
     pub_date = models.DateTimeField(default=timezone.now, editable=False)
     exp_date = models.DateTimeField(default=timezone.now() + timedelta(days=30), editable=False)
 
