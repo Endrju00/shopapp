3ea436fcba2835e49c543f3fe99e2b59c3984713
AM KK <75850182+Endrju00@users.noreply.github.com>
2021-08-12 13:52:48 +0200

    Profile and signal created
    When User object is saved, profile object is created

--- shop/accounts/models.py
+++ shop/accounts/models.py
@@ -1,3 +1,15 @@
 from django.db import models
+from django.contrib.auth.models import User
+from django.core.validators import MaxValueValidator, MinValueValidator
 
 # Create your models here.
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    rating = models.IntegerField(
+        default=5,
+        validators = [MaxValueValidator(5), MinValueValidator(1)]
+    )
+
+
+    def __str__(self):
+        return f'{self.user.username} Profile'
