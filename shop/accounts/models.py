a95e34bd98fda020d9c7c99c7b3c5a373b955ea7
AM KK <75850182+Endrju00@users.noreply.github.com>
2021-08-18 13:23:34 +0200

    Relation between Users and Items added

--- shop/accounts/models.py
+++ shop/accounts/models.py
@@ -2,6 +2,8 @@ from django.db import models
 from django.contrib.auth.models import User
 from django.core.validators import MaxValueValidator, MinValueValidator
 
+from items.models import SaleOffer
+
 # Create your models here.
 class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
@@ -10,5 +12,17 @@ class Profile(models.Model):
         validators = [MaxValueValidator(5), MinValueValidator(1)]
     )
 
+    cart_items = models.ManyToManyField(
+        SaleOffer,
+        through='CartMembership',
+    )
+
     def __str__(self):
         return f'{self.user.username} Profile'
+
+class CartMembership(models.Model):
+    profile = models.ForeignKey(Profile, on_delete=models.CASCADE)
+    item = models.ForeignKey(SaleOffer, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f'{self.profile.user}-{self.item.item.name}'
