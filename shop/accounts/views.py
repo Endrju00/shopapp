9436a2730aefa5a1a3823a2a037aa0b733a48433
AM KK <75850182+Endrju00@users.noreply.github.com>
2021-08-22 15:00:06 +0200

    Changed cart style

--- shop/accounts/views.py
+++ shop/accounts/views.py
@@ -4,6 +4,7 @@ from django.contrib import messages
 from .forms import UserRegisterForm
 from django.contrib.auth.models import User
 from django.contrib.auth.decorators import login_required
+from django.core.paginator import Paginator
 
 from .models import Profile
 from items.models import SaleOffer, Item
@@ -43,9 +44,16 @@ def profile(request, user_id):
 @login_required
 def cart(request):
     user_profile = Profile.objects.get(user=request.user)
+    sales = user_profile.cart_items.all()
+
+    paginator = Paginator(sales, 6)
+    page_number = request.GET.get('page')
+    page_obj = paginator.get_page(page_number)
+
     context = {
-        'sales': user_profile.cart_items.all(),
+        'sales': sales,
         'cart_items': Profile.objects.get(user=request.user).cart_items.all(),
+        'page_obj': page_obj,
     }
 
     context['total_sum'] = round(sum(sale.price for sale in context['cart_items']),2)
